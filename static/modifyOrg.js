4d588d622fff8becfe41889dbae3d32bd069d4ea
D Massey <dmassey@hilobit.com>
2019-11-10 11:44:17 -0800

    start vue

--- static/modifyOrg.js
+++ static/modifyOrg.js
@@ -1,3 +1,14 @@
 function loadUserList() {
     alert('hello world');
 }
+
+var example1 = new Vue({
+    el: '#example-1',
+    delimiters: ['${', '}'],
+    data: {
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' }
+      ]
+    }
+  })
