7d64b6fb7d40f88aeb28c20f6c38a5b836350713
D Massey <dmassey@hilobit.com>
2020-02-09 17:20:48 -0600

    update script to allow ep2 to connect

--- scripts/init-user-db.sh
+++ scripts/init-user-db.sh
@@ -4,8 +4,13 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER ep2 WITH PASSWORD 'ep2';
     CREATE DATABASE enterpriseportal2;
-    GRANT ALL PRIVILEGES ON DATABASE enterpriseportal2 TO ep2;
+    GRANT CONNECT ON DATABASE enterpriseportal2 TO ep2;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "enterpriseportal2" <<-EO1SQL
+    GRANT SELECT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO ep2;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO ep2;
+EO1SQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "enterpriseportal2" < /tmp/00schema.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "enterpriseportal2" < /tmp/01seed.sql
