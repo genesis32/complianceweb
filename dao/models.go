8a06e39750b46d8bb4199e63e12868b8ae7a91b7
D Massey <dmassey@hilobit.com>
2020-04-23 20:35:12 -0500

    model linter fixes.

--- dao/models.go
+++ dao/models.go
@@ -5,6 +5,7 @@ import (
 	"log"
 )
 
+// RegisteredResource is the entity which determines which resources the app is protecting
 type RegisteredResource struct {
 	ID          int64
 	DisplayName string
@@ -12,6 +13,7 @@ type RegisteredResource struct {
 	Enabled     bool
 }
 
+// Organization is used as part of a tree to determine permissions
 type Organization struct {
 	ID          int64
 	DisplayName string
@@ -19,6 +21,8 @@ type Organization struct {
 	Users       []*OrganizationUser
 }
 
+// OrganizationUser is the user that is part of an organization
+// it also contains the explicit roles a user has in each suborganization.
 type OrganizationUser struct {
 	ID            int64
 	DisplayName   string
@@ -27,29 +31,34 @@ type OrganizationUser struct {
 	UserRoles     UserRoleStore
 }
 
+// UserRole describes the roles a user has in an organization
 type UserRole struct {
 	OrganizationID int64
 	RoleNames      []string
 }
 
+// Role is just a collection of permissions
 type Role struct {
 	ID          int64
 	DisplayName string
 }
 
+// Setting contains just a key value mapping of settings for the app
 type Setting struct {
 	Key   string
 	Value string
 }
 
+// Base64EncodeValue just base64 encodes the settings value.
 func (s *Setting) Base64EncodeValue(val []byte) {
 	s.Value = base64.StdEncoding.EncodeToString(val)
 }
 
+// Base64DecodeValue just base64 decodes the settings value.
 func (s *Setting) Base64DecodeValue() []byte {
 	ret, err := base64.StdEncoding.DecodeString(s.Value)
 	if err != nil {
-		log.Fatalf("cannot decode key %s: %w", s.Key, err)
+		log.Fatalf("cannot decode key %s: %v", s.Key, err)
 	}
 	return ret
 }
