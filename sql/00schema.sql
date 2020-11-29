0090dfae80e29605c45e0082dcca953be35e4242
D Massey <dmassey@hilobit.com>
2020-11-29 04:45:13 -0600

    remove stuff

--- sql/00schema.sql
+++ sql/00schema.sql
@@ -14,13 +14,6 @@ organization
 CREATE INDEX IF NOT EXISTS path_gist_idx ON organization USING gist(path);
 CREATE INDEX IF NOT EXISTS path_idx ON organization USING btree(path);
 
-/*
-User: map[aud:***REMOVED*** exp:1.573055384e+09 family_name:Massey given_name:David iat:1.573019384e+09 
-iss:https://***REMOVED***.auth0.com/ locale:en name:David Massey nickname:dmassey 
-picture:httpse//lh3.googleusercontent.com/a-/AAuE7mAO-D5x6lVa_jy2zUP7D6IFz9O7yOTiIHwC-O2_ 
-sub:google-oauth2|111861164484074538139 updated_at:2019-11-06T05:49:43.694Z]
- */
-
 CREATE TABLE IF NOT EXISTS
 organization_organization_user_xref
 ( 
